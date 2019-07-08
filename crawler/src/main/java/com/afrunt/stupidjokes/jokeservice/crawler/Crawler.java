0c8cd7d0c20f554544e033594e7cad9556326568
Andrii Frunt <andrey.frunt@gmail.com>
2019-07-08 12:01:42 +0300

    init

--- crawler/src/main/java/com/afrunt/stupidjokes/jokeservice/crawler/Crawler.java
+++ crawler/src/main/java/com/afrunt/stupidjokes/jokeservice/crawler/Crawler.java
@@ -1,10 +1,22 @@
 package com.afrunt.stupidjokes.jokeservice.crawler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 /**
  * @author Andrii Frunt
@@ -12,6 +24,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableBatchProcessing
 @SpringBootApplication
 public class Crawler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Crawler.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Crawler.class, args);
     }
@@ -19,5 +33,23 @@ public class Crawler {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean(name = "hostsOverrides")
+    public Map<String, String> hostsOverrides() throws IOException {
+        Path path = Paths.get("/deployment/config/hosts.overrides.properties");
+        File file = path.toFile();
+        if (!file.exists() || file.isDirectory() || !file.canRead()) {
+            return Collections.emptyMap();
+        }
+
+        Properties props = new Properties();
+        props.load(new FileInputStream(file));
+
+        LOGGER.info("Hosts overrides found");
+
+        return props.entrySet().stream()
+                .peek(e -> LOGGER.info("{} -> {}", e.getKey(), e.getValue()))
+                .collect(Collectors.toMap(e -> (String) e.getKey(), e -> (String) e.getValue()));
     }
 }
--- crawler/src/main/java/com/afrunt/stupidjokes/jokeservice/crawler/randomjokecrawler/RandomJokeCrawlerJobConfiguration.java
+++ crawler/src/main/java/com/afrunt/stupidjokes/jokeservice/crawler/randomjokecrawler/RandomJokeCrawlerJobConfiguration.java
@@ -4,9 +4,12 @@ import com.afrunt.randomjoke.Jokes;
 import com.afrunt.stupidjokes.jokeservice.crawler.GenericJobFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.Map;
 
 /**
  * @author Andrii Frunt
@@ -18,9 +21,10 @@ public class RandomJokeCrawlerJobConfiguration {
     private GenericJobFactory genericJobFactory;
 
     @Bean
-    public Jokes jokes() {
+    public Jokes jokes(@Qualifier("hostsOverrides") Map<String, String> hostsOverrides) {
         return new Jokes()
-                .withDefaultSuppliers();
+                .withDefaultSuppliers()
+                .overrideHosts(hostsOverrides);
     }
 
     @Bean
